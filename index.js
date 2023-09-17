cbcf83113ab48290daa0c8815c009aac9b90836d
varunSalat <varun.salat@thegatewaycorp.co.in>
2023-09-17 19:37:39 +0530

    Ntl URL added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const filter = {};
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5173/",
+  origin: "https://scholarwithtech.netlify.app/",
   credentials: true, // Allow credentials (cookies, etc.)
 };
 
